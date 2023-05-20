5a3034186686ce088cca93b74d6cd94af5822a26
Timothy Tandt <Timothy.tandt@outlook.be>
2023-05-20 22:27:57 +0200

    Implemented code for file convertion
    You can now convert your file into a png. It will automatically be in your download folder.

---
+++ PicTrans/wwwroot/scripts.js
@@ -0,0 +1,8 @@
+﻿
+function downloadFile(base64Content, filename) {
+    const link = document.createElement('a');
+    link.href = "data:application/octet-stream;base64," + base64Content;
+    link.download = filename;
+
+    link.click();
+}
