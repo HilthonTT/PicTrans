b317929d59627b6054b93bce32135019c453e573
Timothy Tandt <Timothy.tandt@outlook.be>
2023-05-21 19:01:06 +0200

    Implemented MutBlazor on Index.razor. Implemented SecureStorageWrapper.cs Removed ImageData

--- PicTrans/Services/IImageData.cs
+++
@@ -1,9 +0,0 @@
-﻿using PicTrans.Models;
-
-namespace PicTrans.Services;
-public interface IImageData
-{
-    Task<int> AddImageAsync(ImageModel image);
-    Task<int> DeleteImage(ImageModel image);
-    Task<List<ImageModel>> GetImagesAsync();
-}
